e0e96df34b9542b550e755b671bdb3380a77e753
Yukio Arie <yukioarie@gmail.com>
2024-12-12 23:33:16 -0300

    Add i18n

--- app/models/deal_assignee.rb
+++ app/models/deal_assignee.rb
@@ -2,5 +2,5 @@ class DealAssignee < ApplicationRecord
   belongs_to :deal
   belongs_to :user
 
-  validates :user_id, uniqueness: { scope: :deal_id, message: 'já está associado a este negócio' }
+  validates :user_id, uniqueness: { scope: :deal_id, message: :taken }
 end
